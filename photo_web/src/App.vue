a2eb4c75b72aae9e81c812f9faed02ae7cacc8df
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-04-08 23:38:13 -0300

    axios初始化

--- photo_web/src/App.vue
+++ photo_web/src/App.vue
@@ -1,23 +1,36 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+  },
+  mounted: function (){
+    this.getdata();
+    
+  },
+  methods:{
+    getdata(){
+      this.axios
+        .get(
+        )
+        .then((response) => {
+          
+
+          
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
   }
 }
 </script>
--- photo_web/src/main.js
+++ photo_web/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import installElementPlus from './plugins/element'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
 installElementPlus(app)
-app.use(router).use(router).mount('#app')
+app.use(router).use(VueAxios, axios).mount('#app')
