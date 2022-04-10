56974e1801bde4faabcdec5f7969bdae401d9e72
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-04-10 11:57:17 -0300

    node连接

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,8 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.post('/', function (req, res, next) {
-	console.log(req.body.username)
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 })
 
 module.exports = router
