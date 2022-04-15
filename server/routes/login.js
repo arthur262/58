79b154872dcc6e6861591a7aa6fed93c93a70710
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-04-14 21:29:55 -0300

    moongose连接成功

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,23 @@
 var express = require('express')
 var router = express.Router()
+const mongoose = require('mongoose')
 
-/* GET home page. */
+const url = 'mongodb+srv://58_user1:1234567891@cluster0.ui0qo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+
+ /* GET home page. */
 router.post('/', function (req, res, next) {
 	console.log(req.body)
 })
