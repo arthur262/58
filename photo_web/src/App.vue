035002df0f782904bc9f7fc1594e9fd194bbb6f7
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-04-15 20:34:30 -0300

    主页1

--- photo_web/src/App.vue
+++ photo_web/src/App.vue
@@ -2,16 +2,20 @@
   <div style="width:fit-content;
   min-height:100vh;
   margin:0 auto;">
+  <div style="margin:0 auto; width:fit-content;">
+  <Navigate/>
+  </div>
    <router-view />
   </div>
  
 </template>
 
 <script>
+import Navigate from '@/components/NavBar.vue';
 export default {
   name: 'App',
   components: {
-   
+   Navigate
   },
   data() {
     return {
@@ -47,10 +51,9 @@ export default {
   --el-color-primary:#FFB52E !important;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Robot,Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin:0;
   padding:0;
---
+++ photo_web/src/components/Buy&Sell/Quick_guide.vue
@@ -0,0 +1,101 @@
+<template>
+  <h3 style="margin-left: 4%">快速导航</h3>
+  <div class="container shadow_lisght" ref="quick_box">
+    <el-row>
+      <el-col :span="5">
+          <div style="margin-top:2ch;">
+        <el-menu
+          default-active=""
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-menu-item index="1">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator One</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator One</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator One</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator One</span>
+          </el-menu-item>
+          
+          
+        </el-menu>
+          </div>
+      </el-col>
+      <el-col :span="19">
+        <el-carousel
+          :interval="5000"
+          arrow="always"
+          style="border-radius: 1.5ch"
+        >
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import "@/css/shadow.css";
+import { is_phone } from "@/js/phone_test.js";
+import { ref } from "vue";
+
+export default {
+  setup: function () {
+    const quick_box = ref(null);
+    const getDom = () => {
+      quick_box.value.style = "min-width:20ch";
+    };
+    return { quick_box, getDom };
+  },
+  mounted: function () {
+    this.testwidth();
+  },
+  methods: {
+    handleSelect(keyPath) {
+      this.activeIndex = keyPath;
+    },
+    testwidth() {
+      if (!is_phone()) {
+        this.getDom();
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  padding: 1.5ch;
+  border-radius: 1.5ch;
+  min-width: 100ch;
+  margin: 0 1ch;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
