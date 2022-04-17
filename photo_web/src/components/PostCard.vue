2839c36e11b72b17df4ae0d51d3164829262cb9f
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-04-17 16:57:13 -0300

    首页完成

---
+++ photo_web/src/components/PostCard.vue
@@ -0,0 +1,168 @@
+<template>
+<el-card
+      shadow="hover"
+      class="box-card poster_box"
+      v-for="item in msg"
+      :key="item.title"
+      style="border-radius: 1.5ch;"
+    >
+      <!-- 如果当前的poster只有一张的图片 -->
+      <el-row v-if="getJsonLength(item.picture) < 2">
+        
+        <el-col :span="8">
+          <el-image
+            :src="item.picture[0]"
+            loading="lazy"
+            style="border-radius: 1ch"
+          />
+        </el-col>
+        <!-- 显示文字 -->
+        <el-col :span="16">
+          <div class="card_detail">
+            <h3 style="font-weight: bold; margin:0.5ch 0.5ch 0.5ch 0ch">
+              {{ item.title }}
+            </h3>
+            <el-row style="line-height:0.5em;">
+                <el-col :span="16" style="display:flex;">
+                    <p style="font-weight: bold;margin:auto 0.5ch; color:var(--el-color-primary);">
+                    {{item.label}}
+                    </p>
+                    <p style="color: grey;">
+                    {{item.time}}
+                    </p>
+                    
+                </el-col>
+                <el-col :span="8" >
+                    <p style="float:right; color: grey;"> {{item.location}}</p>
+                </el-col>
+            </el-row>
+
+            <!-- 限制字符串显示长度为20个char -->
+            <p v-if="item.description.length<40">
+            {{item.description}}
+            </p>
+            <p v-else>
+              {{item.description.substring(0,40)}}...
+            </p>
+          </div>
+        </el-col>
+      </el-row>
+
+      <!-- 如果当前的poster是否有4以上的图片 -->
+      <div v-else-if="getJsonLength(item.picture) < 4">
+        
+        <!-- 显示文字 -->
+        
+          <div class="card_detail">
+            <h3 style="font-weight: bold; margin:0.5ch 0.5ch 0.5ch 0ch">
+              {{ item.title }}
+            </h3>
+            <el-row style="line-height:0.5em;">
+                <el-col :span="16" style="display:flex;">
+                    <p style="font-weight: bold;margin-right:1ch; color:var(--el-color-primary);">
+                    {{item.label}}
+                    </p>
+                    <p style="color: grey;">
+                    {{item.time}}
+                    </p>
+                    
+                </el-col>
+                <el-col :span="8" >
+                    <p style="float:right; color: grey;"> {{item.location}}</p>
+                </el-col>
+            </el-row>
+
+            <!-- 限制字符串显示长度为20个char -->
+            <p v-if="item.description.length<40">
+            {{item.description}}
+            </p>
+            <p v-else>
+              {{item.description.substring(0,40)}}...
+            </p>
+          </div>
+          <div  style="display:flex;">
+            <el-image
+            v-for="child in item.picture" :key="child"
+            :src="child"
+            loading="lazy"
+            style="border-radius: 1ch;margin:0.5ch;width:20ch;height:auto;"
+          />
+          </div>
+
+      </div>
+      <!-- 如果当前的poster图片超出范围 -->
+      <div v-else-if="getJsonLength(item.picture)>= 4">
+        
+        <!-- 显示文字 -->
+        
+          <div class="card_detail">
+            <h3 style="font-weight: bold; margin:0.5ch 0.5ch 0.5ch 0ch">
+              {{ item.title }}
+            </h3>
+            <el-row style="line-height:0.5em;">
+                <el-col :span="16" style="display:flex;">
+                    <p style="font-weight: bold;margin-right:1ch; color:var(--el-color-primary);">
+                    {{item.label}}
+                    </p>
+                    <p style="color: grey;">
+                    {{item.time}}
+                    </p>
+                    
+                </el-col>
+                <el-col :span="8" >
+                    <p style="float:right; color: grey;"> {{item.location}}</p>
+                </el-col>
+            </el-row>
+
+            <!-- 限制字符串显示长度为20个char -->
+            <p v-if="item.description.length<40">
+            {{item.description}}
+            </p>
+            <p v-else>
+              {{item.description.substring(0,40)}}...
+            </p>
+          </div>
+          <div  style="display:flex;">
+            <el-image
+            v-for="child in item.picture.slice(0,5)" :key="child"
+            :src="child"
+            loading="lazy"
+            
+            style="border-radius: 1ch;margin:0.5ch;width:20ch;height:auto;"
+          />
+          </div>
+
+      </div>
+    </el-card>
+  
+</template>
+
+<script>
+export default {
+    props: {
+    msg: Array
+  },
+    methods: {
+    getJsonLength(jsonData) {
+      var jsonLength = 0;
+      // eslint-disable-next-line no-unused-vars
+      for (var item in jsonData) {
+        jsonLength++;
+      }
+      return jsonLength;
+    },
+  },
+
+}
+</script>
+
+<style>
+.poster_box {
+  border-radius: 1.5ch;
+  margin: 1.5ch 1ch;
+}
+.card_detail {
+  padding: 0 2ch;
+  
+}
+</style>
